f04d04bb7a773c54fbb9ed6b83eb77335d2c2f56
Pavel Khrebto <rezgo.dev@dataart.com>
2016-06-19 17:11:17 +0200

    capistrano deploy script update

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,10 @@ set :user, 'ubuntu'
 set :rbenv_ruby, '2.3.0'
 
 set :puma_bind, 'tcp://0.0.0.0:9292'
+
+set :domain, '52.29.110.91'
+
+# Roles
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
