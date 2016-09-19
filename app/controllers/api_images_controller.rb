e2739029baf305ad331ea57254a9fc2944db4212
Pavel Khrebto <rezgo.dev@dataart.com>
2016-09-19 20:40:13 +0200

    change structure of getImages response

--- app/controllers/api_images_controller.rb
+++ app/controllers/api_images_controller.rb
@@ -1,5 +1,20 @@
 class ApiImagesController < BaseApiController
   def index
-    render :json => AssetImage.where(:asset_id => params[:asset_id]).all
+    images = AssetImage.where(:asset_id => params[:asset_id]).all
+
+    images_array = Array.new
+
+    images.each do |image|
+      image_hash = Hash.new
+
+      image_hash[:id] = image.id
+      image_hash[:title] = image.title
+      image_hash[:description] = image.description
+      image_hash[:url] = image.image_file.url(:thumb)
+
+      images_array.append(image_hash)
+    end
+
+    render :json => images_array
   end
 end
