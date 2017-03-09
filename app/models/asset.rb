0063ddcf544aa4ee9944bc546d706e86a6327dca
Pavel Khrebto <rezgo.dev@dataart.com>
2017-03-09 22:59:52 +0100

    add m4a audio format

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -9,7 +9,7 @@ class Asset < ActiveRecord::Base
   belongs_to :city
 
   has_attached_file :audio_file
-  validates_attachment_content_type :audio_file, :content_type => ["application/mp3", "application/x-mp3", "audio/mpeg", "audio/mp3"]
+  validates_attachment_content_type :audio_file, :content_type => ["application/mp3", "application/x-mp3", "audio/mpeg", "audio/mp3", "audio/m4a"]
 
   has_attached_file :thumbnail, :required => false, :styles => { :medium => "300x300#", :thumb => "200x200#" }
 
